8cd179b440d9a9f4d6f0c7942dd8152b666bd34b
shaygyawali <shaygyawali@gmail.com>
2024-03-26 16:17:14 -0400

    setting up backend

---
+++ backend/dist/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+// GET - get data from database
+// PUT - update or modify (mutate) data
+// POST - send data 
+// DELETE - remove data
+app.use(express.json());
+app.delete("/hello", (req, res, next) => {
+    console.log(req.body.name);
+    return res.send("Hello");
+});
+app.listen(3000, () => console.log("Server Open"));
+//# sourceMappingURL=index.js.map
